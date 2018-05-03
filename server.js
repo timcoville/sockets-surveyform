3342c6019cd9cb3b4bf6c23dbe360fcb93bbe891
Tim Coville <tcoville@gmail.com>
2018-05-03 13:29:08 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var server = app.listen(1337);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname +'/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
+
+io.on('connection', function(socket){
+    socket.emit('greeting', { msg: 'Greetings, from serverNode, brought to you by Sockets! - Server'})
+    socket.on('thankyou', function(data){
+        console.log(data.msg + "- from client: " + data.id);
+    });
+});
+
